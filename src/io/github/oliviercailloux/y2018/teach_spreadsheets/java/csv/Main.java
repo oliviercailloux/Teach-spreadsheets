dcf970885d60a8223ab19bfac5367fbb25414939
tuannamdavaux <davauxtuannam96@gmail.com>
2018-04-05 21:19:53 +0200

    Ressources1: version 1.1

---
+++ src/io/github/oliviercailloux/y2018/teach_spreadsheets/java/csv/Main.java
@@ -0,0 +1,29 @@
+package io.github.oliviercailloux.y2018.teach_spreadsheets.java.csv;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.oliviercailloux.y2018.teach_spreadsheets.java.courses.Course;
+
+public class Main {
+
+	/**
+	 * @author tuannamdavaux, kantoki <br>
+	 *         </br>
+	 *         Show start-courses.csv content obtained with CsvFileReader.
+	 */
+	public static void main(String[] args) throws NumberFormatException,
+			FileNotFoundException, IllegalArgumentException, IOException {
+		List<Course> courses2 = new ArrayList<>();
+		System.out.println("\n");
+		URL ressourceUrl = Main.class.getResource("start-courses.csv");
+		System.out.println(ressourceUrl.getPath());
+		CsvFileReader.readCourseCSVfile(ressourceUrl.getPath(), courses2);
+		System.out.println(courses2);
+
+	}
+
+}
