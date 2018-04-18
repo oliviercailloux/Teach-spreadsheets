59524573d8cdcd9eede05515fc6ecbaa7cca74f0
fontlo15 <lf94777@gmail.com>
2018-04-18 22:03:32 +0200

    -- modifications

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
@@ -1,16 +1,20 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
 
-import org.odftoolkit.simple.SpreadsheetDocument;
-
 /**
+ * <b>This interface allows to read data from a spreadsheet.</b>
  * 
  * @author tuannamdavaux, samuelcohen75
- *
+ * @see io.github.oliviercailloux.y2018.teach_spreadsheets.odf.SpreadsheetShower
  */
 public interface SpreadsheetReader {
-
+	/**
+	 * get cell value at cellPosition
+	 */
 	public String getCellValue(String cellPosition);
 
-	public boolean isDiagonalBorder(SpreadsheetDocument sd, String yearOfStudy, String cellPosition);
+	/**
+	 * detect if there is a diagonal border in cell at cellPosition
+	 */
+	public boolean isSlash(String cellPosition);
 
 }
