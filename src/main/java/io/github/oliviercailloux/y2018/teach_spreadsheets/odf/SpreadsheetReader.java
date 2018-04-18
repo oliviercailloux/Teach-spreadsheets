0c5b57f58e880b8c27f8ab423ce9d42ed75f42ed
fontlo15 <lf94777@gmail.com>
2018-04-18 21:57:59 +0200

    -- implement interface

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
@@ -1,18 +1,16 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
 
+import org.odftoolkit.simple.SpreadsheetDocument;
+
 /**
  * 
  * @author tuannamdavaux, samuelcohen75
  *
  */
 public interface SpreadsheetReader {
-	
+
 	public String getCellValue(String cellPosition);
-	
-	public boolean isSlash(String cellPosition);
-	
-	public String getCurrentSheet();
-	
-	
+
+	public boolean isDiagonalBorder(SpreadsheetDocument sd, String yearOfStudy, String cellPosition);
 
 }
