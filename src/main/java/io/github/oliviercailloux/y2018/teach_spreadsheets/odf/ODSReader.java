f2f6ad0218c5661bd53ab1439d11000cc64fe565
fontlo15 <lf94777@gmail.com>
2018-04-18 21:23:59 +0200

    -- getCellValue

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReader.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReader.java
@@ -1,11 +1,34 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
 
+import java.util.Objects;
+
+import org.odftoolkit.simple.SpreadsheetDocument;
+import org.odftoolkit.simple.table.Cell;
+import org.odftoolkit.simple.table.Table;
+
 public class ODSReader implements SpreadsheetReader {
+
+	private SpreadsheetDocument document;
+
+	private Table sheet;
+
+	public ODSReader(SpreadsheetDocument spreadsheetDocument) {
+		this.document = Objects.requireNonNull(spreadsheetDocument);
+		this.sheet = Objects.requireNonNull(spreadsheetDocument.getTableList().get(0));
+	}
+
+	public ODSReader(SpreadsheetDocument spreadsheetDocument, String sheet) {
+		this.document = Objects.requireNonNull(spreadsheetDocument);
+		this.sheet = Objects.requireNonNull(spreadsheetDocument.getTableByName(sheet));
+	}
 
 	@Override
 	public String getCellValue(String cellPosition) {
-		TODO();
-		return null;
+		Cell cell = sheet.getCellByPosition(cellPosition);
+		if (cell == null) {
+			return "";
+		}
+		return cell.getDisplayText();
 	}
 
 	@Override
