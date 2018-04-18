fb5e1ac6417c832993b1d46ff1eda4afd944c39f
fontlo15 <lf94777@gmail.com>
2018-04-18 20:37:23 +0200

    -- La base de la classe

---
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReader.java
@@ -0,0 +1,23 @@
+package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
+
+public class ODSReader implements SpreadsheetReader {
+
+	@Override
+	public String getCellValue(String cellPosition) {
+		TODO();
+		return null;
+	}
+
+	@Override
+	public boolean isSlash(String cellPosition) {
+		TODO();
+		return false;
+	}
+
+	@Override
+	public String getCurrentSheet() {
+		TODO();
+		return null;
+	}
+
+}
