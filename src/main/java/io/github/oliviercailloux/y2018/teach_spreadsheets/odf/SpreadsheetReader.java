681e6a214a9d98835cc9c8641aad6828fd395724
tuannamdavaux <davauxtuannam96@gmail.com>
2018-05-03 11:45:12 +0200

    Interfaces1 : methods to specify current sheet and input file for the SPreadsheetReader added.

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetReader.java
@@ -1,6 +1,7 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
+import org.odftoolkit.simple.table.Table;
 
 /**
  * <b>This interface allows to read data from a spreadsheet.</b>
@@ -19,5 +20,16 @@ public interface SpreadsheetReader {
 	 * detect if there is a diagonal border in cell at cellPosition
 	 */
 	public boolean isDiagonalBorder(SpreadsheetDocument sd, String yearOfStudy, String cellPosition);
+	
+	/**
+	 * specify a current sheet
+	 */
+	public void setSheet(Table sheet);
+	
+	/**
+	 * specify the input file
+	 */
+	public void setDocument(SpreadsheetDocument  document);
+	
 
 }
--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetShower.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetShower.java
@@ -6,6 +6,8 @@ package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
 import java.util.Objects;
 import java.util.Scanner;
 
+import org.odftoolkit.simple.SpreadsheetDocument;
+
 /**
  * <b>This class allows to print data by using a SpreadsheetShower object.</b>
  * @author tuannamdavaux
@@ -40,6 +42,29 @@ public class SpreadsheetShower {
 			}else cellValue = spreadsheetReader.getCellValue(position);
 		} while (!typeEnd);
 		}
+	}
+	
+	/**
+	 * Same method plus : 
+	 * This method asks the user to specify a file.
+	 */
+	public void showCellsValueWithSpecifiedFile() throws Exception {
+		try(Scanner sc = new Scanner(System.in)){
+			String position = "";
+			String cellValue = "";
+			boolean typeEnd = false;
+			System.out.println("Veuillez préciser le fichier d'entrée avec le chemin complet:");
+			spreadsheetReader.setDocument(SpreadsheetDocument.loadDocument(sc.nextLine()));
+			System.out.println("Ce programme peut lire les cellules de la feuille :\nEntrez une position par exemple A1 / Tapez Fin pour terminer la saisie.");
+			do {
+				System.out.println(cellValue);
+				position = sc.nextLine();
+				if(position.equals("Fin")) {
+					System.out.println("End");
+					typeEnd = !typeEnd;
+				}else cellValue = spreadsheetReader.getCellValue(position);
+			} while (!typeEnd);
+			}
 	}
 
 }
