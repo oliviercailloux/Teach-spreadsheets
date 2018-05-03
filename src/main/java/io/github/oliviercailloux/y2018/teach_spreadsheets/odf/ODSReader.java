4f70c87fcdb45dad27dc852b4f858943b0d4ddd5
fontlo15 <lf94777@gmail.com>
2018-05-03 09:46:39 +0200

    -- modifications mineures

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReader.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReader.java
@@ -8,11 +8,10 @@ import org.odftoolkit.simple.table.Cell;
 import org.odftoolkit.simple.table.Table;
 
 /**
- * @author Victor CHEN (Kantoki), Louis Fontaine
- * This class implements SpreadsheetReader : it allows to read a Spreadsheet document, get the value of a cell,
- * check if a cell is a diagonal border or not
- * Version : 1.0
- * Last update : 18/04/2018
+ * @author Victor CHEN (Kantoki), Louis Fontaine This class implements
+ *         SpreadsheetReader : it allows to read a Spreadsheet document, get the
+ *         value of a cell, check if a cell is a diagonal border or not Version
+ *         : 1.0 Last update : 18/04/2018
  */
 public class ODSReader implements SpreadsheetReader {
 
@@ -30,6 +29,18 @@ public class ODSReader implements SpreadsheetReader {
 		this.sheet = Objects.requireNonNull(spreadsheetDocument.getTableByName(sheet));
 	}
 
+	public Table getSheet() {
+		return sheet;
+	}
+
+	public void setSheet(Table sheet) {
+		this.sheet = sheet;
+	}
+
+	public void setDocument(SpreadsheetDocument document) {
+		this.document = Objects.requireNonNull(document);
+	}
+
 	@Override
 	public String getCellValue(String cellPosition) {
 		Cell cell = sheet.getCellByPosition(cellPosition);
@@ -42,9 +53,10 @@ public class ODSReader implements SpreadsheetReader {
 	@Override
 	public boolean isDiagonalBorder(SpreadsheetDocument sd, String yearOfStudy, String cellPosition) {
 		Table sheet = sd.getSheetByName(yearOfStudy);
-		/* There is a problem with ODFTookit, their function getBorder return NULL
-		 * if the border doesn't exists, but if there is a border, It doesn't return
-		 * the description but a NumberFormatException, so the catch fix it
+		/*
+		 * There is a problem with ODFTookit, their function getBorder return NULL if
+		 * the border doesn't exists, but if there is a border, It doesn't return the
+		 * description but a NumberFormatException, so the catch fix it
 		 */
 		try {
 			sheet.getCellByPosition(cellPosition).getBorder(CellBordersType.DIAGONALBLTR);
