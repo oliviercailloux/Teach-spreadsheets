0aae4f56a6b8d3db888a403d77dc04cd0345acaa
Kantoki <victorcdauphine@gmail.com>
2018-05-07 19:45:46 +0200

    -- New tests

---
+++ src/test/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ODSReaderTest.java
@@ -0,0 +1,74 @@
+package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.odftoolkit.simple.SpreadsheetDocument;
+
+public class ODSReaderTest {
+
+	/**
+	 * Test to check that the method works as a cell (F4) is a diagonal border
+	 */
+	@Test
+	public void testCellIsDiagonalBorder() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		ODSReader odsR = new ODSReader(sd);
+
+		String yearOfStudy = "L3_Informatique";
+		String cellPosition = "F4";
+
+		Assert.assertTrue(odsR.isDiagonalBorder(sd, yearOfStudy, cellPosition));
+
+	}
+
+	/**
+	 * Test to check that the method works as a cell (I4) is not a diagonal border
+	 */
+	@Test
+	public void testCellIsNotDiagonalBorder() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		ODSReader odsR = new ODSReader(sd);
+
+		String yearOfStudy = "L3_Informatique";
+		String cellPosition = "I4";
+
+		Assert.assertFalse(odsR.isDiagonalBorder(sd, yearOfStudy, cellPosition));
+	}
+
+	/**
+	 * Test to check we can retrieve a cell value
+	 */
+	@Test
+	public void testCellValueOK() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		String yearOfStudy = "L3_Informatique";
+		ODSReader odsR = new ODSReader(sd, yearOfStudy);
+
+		String cellPosition = "I5";
+
+		Assert.assertEquals("2", odsR.getCellValue(cellPosition));
+	}
+
+	/**
+	 * Test to check we can retrieve a cell value
+	 */
+	@Test
+	public void testCellValueNotOK() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		String yearOfStudy = "L3_Informatique";
+		ODSReader odsR = new ODSReader(sd, yearOfStudy);
+
+		String cellPosition = "I4";
+
+		Assert.assertEquals("", odsR.getCellValue(cellPosition));
+	}
+
+}
