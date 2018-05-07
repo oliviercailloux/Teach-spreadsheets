24a5ded4024cb5d237d6d2c36d76e5380358ef86
Kantoki <victorcdauphine@gmail.com>
2018-05-07 20:11:38 +0200

    -- Start ReadCourses class

---
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ReadCourses.java
@@ -0,0 +1,23 @@
+package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
+
+import java.util.Scanner;
+
+public class ReadCourses {
+	
+	private List<Course> courses;
+	
+	public List<Course> readCourses() {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] beginCell;
+		String cell = sc.next();
+		
+		while (beginCell != "STOP") {
+			
+		}
+		
+		return courses;
+		
+	}
+
+}
