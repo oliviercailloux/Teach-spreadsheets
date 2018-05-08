c063ecfb1bd3d71114171208c97f6a47fabbef9b
Kantoki <victorcdauphine@gmail.com>
2018-05-08 20:33:35 +0200

    -- JUnits for ReadCourses

---
+++ src/test/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/ReadCoursesTest.java
@@ -0,0 +1,73 @@
+package io.github.oliviercailloux.y2018.teach_spreadsheets.odf;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.Test;
+import org.odftoolkit.simple.SpreadsheetDocument;
+
+import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Course;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+
+public class ReadCoursesTest {
+	
+	/**
+	 * Test to check that the method iterates on all courses of the sheet
+	 */
+	@Test
+	public void testReadCourses() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		String yearOfStudy = "L3_Informatique";
+		ODSReader odsR = new ODSReader(sd, yearOfStudy);
+		
+		ReadCourses rd = new ReadCourses(odsR);
+		List<Course> courses;
+		courses = rd.readCourses("B4");
+		int nbCourses = courses.size();
+		
+		Assert.assertEquals(11, nbCourses);
+	}
+	
+	/**
+	 * Test to check that the method retrives the right courses
+	 */
+	@Test
+	public void testSameCourseSheetAndReadCourses() throws Exception {
+		InputStream is = ODSReaderTest.class.getClassLoader().getResourceAsStream("Saisie_voeux_dauphine.ods");
+		SpreadsheetDocument sd = SpreadsheetDocument.loadDocument(is);
+		String yearOfStudy = "L3_Informatique";
+		ODSReader odsR = new ODSReader(sd, yearOfStudy);
+		
+		ReadCourses rd = new ReadCourses(odsR);
+		List<Course> courses;
+		courses = rd.readCourses("B4");
+		
+		Course c1 = new Course();
+		for (int i = 1; i < courses.size(); i++) {
+			c1 = courses.get(0);
+		}
+		
+		Course c2 = new Course();
+		c2.setName("Pré-rentrée : Mise à niveau Probabilités");
+		c2.setYearOfStud("L3_Informatique");
+		c2.setapogeeCode("A3MANPRO");
+		c2.setSupervisor("Mayag");
+		c2.setTeachers("Mayag");
+		c2.setCM_Hour(0);
+		c2.setTD_Hour(0);
+		c2.setCMTD_Hour(15);
+		c2.setTP_Hour(0);
+		c2.setNbGrpCM(0);
+		c2.setNbGrpCMTD(0);
+		c2.setNbGrpTD(0);
+		c2.setNbGrpTP(0);
+		
+		Assertions.assertThat(c1).isEqualToComparingFieldByFieldRecursively(c2);
+	}
+
+}
