9aee47bc64bad060fac142c53787b99bd47dba30
Kantoki <victorcdauphine@gmail.com>
2018-05-29 12:55:18 +0200

    -- Logo GUI working

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
@@ -1,60 +1,78 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.gui;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Teacher;
 
 public class GUIPref {
-	public static void main(String[] args) {
+	private final static Logger LOGGER = LoggerFactory.getLogger(GUIPref.class);
 
-		// setup the SWT window
-		Display display = new Display();
-		Shell shell = new Shell(display, SWT.RESIZE | SWT.CLOSE | SWT.MIN);
-		shell.setText("Conference");
+	private void initializeMainMenu() throws IOException {
 
-		// initialize a grid layout manager
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		shell.setLayout(gridLayout);
+		String logoFileName = "logoGUI.png";
 
-		// create the label and the field text
-		Label labelTitle = new Label(shell, SWT.NONE);
-		labelTitle.setText("Title : ");
-		Text textTitle = new Text(shell, SWT.BORDER);
+		try (InputStream inputStream = GUIPref.class.getResourceAsStream(logoFileName)) {
+			if (inputStream == null) {
+				LOGGER.error("File " + logoFileName + " not found.");
+				throw new FileNotFoundException("File not found");
+			}
 
-		Button buttonSubmit = new Button(shell, SWT.PUSH);
-		buttonSubmit.setText("Submit");
-		buttonSubmit.addSelectionListener(new SelectionAdapter() {
-			// the function proceed() executed once we press the button Submit
-			// @Override
-			/*
-			 * public void widgetSelected(SelectionEvent event) { try {
-			 * createPref(); } catch (ValidationException e) {
-			 * e.printStackTrace(); } catch (ParseException e) {
-			 * e.printStackTrace(); } catch (IOException e) {
-			 * e.printStackTrace(); } catch (ParserException e) {
-			 * e.printStackTrace(); } catch (URISyntaxException e) {
-			 * e.printStackTrace(); }
-			 * 
-			 * }
-			 */
+			Display display = new Display();
 
-			// this function save the value in the fields of GUI in a conference
-			// and
-			// write-read a ICalendar
-			/*
-			 * private void createPref() { List<CoursePref> prefs = new
-			 * ArrayList<CoursePref>();
-			 * 
-			 * }
-			 */
-		});
+			Image image = new Image(display, inputStream);
 
+			Shell shell = new Shell(display);
+			shell.setText("Menu principal - Teach-spreadsheets");
+			shell.setLayout(new GridLayout(1, false));
+			shell.setSize(800, 800);
+			Label labelImg = new Label(shell, SWT.BORDER);
+			Rectangle clientArea = shell.getClientArea();
+			labelImg.setLocation(clientArea.x, clientArea.y);
+			labelImg.setImage(image);
+			labelImg.pack();
+
+			// Create a horizontal separator
+			Label separator = new Label(shell, SWT.HORIZONTAL | SWT.SEPARATOR);
+			separator.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+			// Create a label
+			new Label(shell, SWT.NONE).setText("This is a plain label.");
+
+			Text text = new Text(shell, SWT.BORDER);
+			text.setText("aaaa");
+
+			// shell.pack ();
+			shell.open();
+			while (!shell.isDisposed()) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+			image.dispose();
+			display.dispose();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		GUIPref gui = new GUIPref();
+		gui.initializeMainMenu();
 	}
 
 }
