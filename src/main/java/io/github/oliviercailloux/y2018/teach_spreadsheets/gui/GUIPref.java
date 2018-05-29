3c09c596201e12a987bdbbfeac80c1fe71160ecc
Kantoki <victorcdauphine@gmail.com>
2018-05-29 13:22:33 +0200

    -- Retrieving teacher infos works

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
@@ -1,8 +1,14 @@
 package io.github.oliviercailloux.y2018.teach_spreadsheets.gui;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -20,7 +26,10 @@ import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Course;
 import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Teacher;
+import io.github.oliviercailloux.y2018.teach_spreadsheets.csv.CsvFileReader;
+import io.github.oliviercailloux.y2018.teach_spreadsheets.csv.CsvFileReaderTest;
 
 public class GUIPref {
 	private final static Logger LOGGER = LoggerFactory.getLogger(GUIPref.class);
@@ -35,12 +44,14 @@ public class GUIPref {
 				throw new FileNotFoundException("File not found");
 			}
 
+			LOGGER.info("Image bien récupérée");
+
 			Display display = new Display();
 			Shell shell = new Shell(display);
 			shell.setText("Menu principal - Teach-spreadsheets");
 			shell.setLayout(new GridLayout(1, false));
 			shell.setSize(500, 700);
-			
+
 			// Display an image
 			Image image = new Image(display, inputStream);
 			Label labelImg = new Label(shell, SWT.BORDER);
@@ -55,13 +66,13 @@ public class GUIPref {
 
 			// Label with teacher name
 			Label lblCentered = new Label(shell, SWT.NONE);
-			lblCentered.setText("Bienvenue");
+			lblCentered.setText("Bienvenue" + getTheTeacher());
 			lblCentered.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, false, false));
-			
+
 			// Create a horizontal separator
 			Label separator2 = new Label(shell, SWT.HORIZONTAL | SWT.SEPARATOR);
 			separator2.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-			
+
 			Text text = new Text(shell, SWT.BORDER);
 			text.setText("aaaa");
 
@@ -73,6 +84,27 @@ public class GUIPref {
 			}
 			image.dispose();
 			display.dispose();
+		}
+	}
+
+	private String getTheTeacher()
+			throws NumberFormatException, FileNotFoundException, IllegalArgumentException, IOException {
+
+		String teacherDetails = "teacher_info.csv";
+
+		try (InputStream inputStream = GUIPref.class.getResourceAsStream(teacherDetails)) {
+			if (inputStream == null) {
+				LOGGER.error("File " + teacherDetails + " not found.");
+				throw new FileNotFoundException("File not found");
+			}
+
+			Reader reader = new InputStreamReader(inputStream);
+			Teacher t = new Teacher();
+			t = CsvFileReader.readTeacherFromCSVfile(reader);
+
+			LOGGER.info("Les informations du Teacher ont bien été récupérées !");
+			String teacherName = t.getFirstName() + t.getName();
+			return teacherName;
 		}
 	}
 
