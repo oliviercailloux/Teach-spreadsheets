4bfcbc0478f21b9411b5caea939c28f69402aa71
Kantoki <victorcdauphine@gmail.com>
2018-05-30 11:31:43 +0200

    -- Child (pref) shell created

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/gui/GUIPref.java
@@ -8,10 +8,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
@@ -20,16 +23,18 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Course;
 import io.github.oliviercailloux.y2018.teach_spreadsheets.courses.Teacher;
 import io.github.oliviercailloux.y2018.teach_spreadsheets.csv.CsvFileReader;
-import io.github.oliviercailloux.y2018.teach_spreadsheets.csv.CsvFileReaderTest;
 
 public class GUIPref {
 	private final static Logger LOGGER = LoggerFactory.getLogger(GUIPref.class);
@@ -44,19 +49,19 @@ public class GUIPref {
 				throw new FileNotFoundException("File not found");
 			}
 
-			LOGGER.info("Image bien récupérée");
+			LOGGER.info("Image-Logo bien récupérée");
 
 			Display display = new Display();
-			Shell shell = new Shell(display);
+			Shell shell = new Shell(display, SWT.CLOSE);
 			shell.setText("Menu principal - Teach-spreadsheets");
 			shell.setLayout(new GridLayout(1, false));
 			shell.setSize(500, 700);
 
 			// Display an image
 			Image image = new Image(display, inputStream);
-			Label labelImg = new Label(shell, SWT.BORDER);
+			Label labelImg = new Label(shell, SWT.CENTER);
 			Rectangle clientArea = shell.getClientArea();
-			labelImg.setLocation(clientArea.x, clientArea.y);
+			// labelImg.setLocation(clientArea.x, clientArea.y);
 			labelImg.setImage(image);
 			labelImg.pack();
 
@@ -73,10 +78,31 @@ public class GUIPref {
 			Label separator2 = new Label(shell, SWT.HORIZONTAL | SWT.SEPARATOR);
 			separator2.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
+			Button button = new Button(shell, SWT.NONE);
+			button.setText("Créez mes préférences");
+			button.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+			button.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					// =====================================================
+					// on button press, create a child Shell object passing
+					// the main Display. The child could also access the
+					// display itself by calling Display.getDefault()
+					// =====================================================
+					LOGGER.info("Shell for the courses preferences well opened");
+					prefShell(display);
+				}
+
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					widgetSelected(e);
+				}
+			});
+
 			Text text = new Text(shell, SWT.BORDER);
 			text.setText("aaaa");
 
-			// shell.pack ();
+			shell.pack();
 			shell.open();
 			while (!shell.isDisposed()) {
 				if (!display.readAndDispatch())
@@ -84,9 +110,17 @@ public class GUIPref {
 			}
 			image.dispose();
 			display.dispose();
+			LOGGER.info("Display well closed");
+
 		}
 	}
 
+	/**
+	 * This method allows to get the teacher's name from reading a CSV file
+	 * containing all his infos like address, phone number...
+	 * 
+	 * @return the teacher's names
+	 */
 	private String getTheTeacher()
 			throws NumberFormatException, FileNotFoundException, IllegalArgumentException, IOException {
 
@@ -106,6 +140,50 @@ public class GUIPref {
 			String teacherName = t.getFirstName() + t.getName();
 			return teacherName;
 		}
+	}
+
+	private void prefShell(Display display) {
+		
+		// Doesn't allow the user to close the main shell when the child shell is open
+		final Shell prefShell = new Shell(display, SWT.CLOSE | SWT.SYSTEM_MODAL);
+		prefShell.setLayout(new GridLayout(1, false));
+		prefShell.setText("Mes préférences - Teach-spreadsheets");
+
+		// ============================
+		// Create a Label in the Shell
+		// ============================
+		Label topLabel = new Label(prefShell, SWT.NONE);
+		topLabel.setText("Insérez vos préférences");
+		topLabel.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, false, false));
+
+		// Create a horizontal separator
+		Label separator = new Label(prefShell, SWT.HORIZONTAL | SWT.SEPARATOR);
+		separator.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		
+		
+		final List list = new List (prefShell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+
+		// shell.pack();
+		prefShell.open();
+
+		// =============================================================
+		// Register a listener for the Close event on the child Shell.
+		// This disposes the child Shell
+		// =============================================================
+		prefShell.addListener(SWT.Close, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				LOGGER.info("Shell for the courses preferences well closed");
+				prefShell.dispose();
+			}
+		});
+	}
+	
+	private ArrayList<Course> getCoursesFromSemester() {
+		ArrayList<Course> courses = new ArrayList();
+		
+		return courses;
 	}
 
 	public static void main(String[] args) throws IOException {
