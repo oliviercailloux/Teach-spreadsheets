be38a9657cc1d0de05fa5ba7e7b40148af256fae
fontlo15 <lf94777@gmail.com>
2018-05-31 16:42:33 +0200

    SpreadsheetShower

--- src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetShower.java
+++ src/main/java/io/github/oliviercailloux/y2018/teach_spreadsheets/odf/SpreadsheetShower.java
@@ -21,10 +21,10 @@ public class SpreadsheetShower {
 	/**
 	 * ODSReader object for showing cell values
 	 */
-	private ODSReader reader = null;
+	private SpreadsheetReader reader = null;
 
-	public SpreadsheetShower(ODSReader ODSReader) {
-		this.reader = Objects.requireNonNull(ODSReader);
+	public SpreadsheetShower(SpreadsheetReader spreadSheetReader) {
+		this.reader = Objects.requireNonNull(spreadSheetReader);
 	}
 
 	/**
@@ -32,46 +32,50 @@ public class SpreadsheetShower {
 	 * its value. And it will ask again until the user types End.
 	 */
 	@SuppressWarnings("resource")
-	public void showCellsValue(boolean fileNeeded) {
+	public void showCellValue(boolean fileNeeded) {
 
-		try (Scanner sc = new Scanner(System.in)) {
-			Table currentSheet = null;
-			String position = "";
-			String cellValue = "";
-			boolean typeEnd = false;
-			InputStream file;
-			if (fileNeeded) {
-				do {
+		if (reader instanceof ODSReader) {
+			ODSReader odsReader = (ODSReader) reader;
+
+			try (Scanner sc = new Scanner(System.in)) {
+				Table currentSheet = null;
+				String position = "";
+				String cellValue = "";
+				boolean typeEnd = false;
+				InputStream file;
+				if (fileNeeded) {
+					do {
+						try {
+							System.out.println("Veuillez préciser le fichier d'entrée avec le chemin complet:");
+							file = new FileInputStream(sc.nextLine());
+						} catch (@SuppressWarnings("unused") FileNotFoundException e) {
+							file = null;
+						}
+					} while (file == null);
+
 					try {
-						System.out.println("Veuillez préciser le fichier d'entrée avec le chemin complet:");
-						file = new FileInputStream(sc.nextLine());
-					} catch (@SuppressWarnings("unused") FileNotFoundException e) {
-						file = null;
+						odsReader.setDocument(file);
+					} catch (Exception e) {
+						throw new IllegalStateException(e);
 					}
-				} while (file == null);
-
-				try {
-					reader.setDocument(file);
-				} catch (Exception e) {
-					throw new IllegalStateException(e);
 				}
-			}
-			System.out.println(
-					"Ce programme peut lire les cellules de la feuille :\nEntrez une position par exemple A1 / Tapez Fin pour terminer la saisie.");
-			do {
-				System.out.println("Précisez la feuille.");
-				currentSheet = this.reader.getTable(sc.nextLine());
-			} while (currentSheet == null);
-			do {
-				System.out.println(cellValue);
+				System.out.println(
+						"Ce programme peut lire les cellules de la feuille :\nEntrez une position par exemple A1 / Tapez Fin pour terminer la saisie.");
+				do {
+					System.out.println("Précisez la feuille.");
+					currentSheet = odsReader.getTable(sc.nextLine());
+				} while (currentSheet == null);
+				do {
+					System.out.println(cellValue);
 
-				position = sc.nextLine();
-				if (position.equals("Fin")) {
-					System.out.println("End");
-					typeEnd = !typeEnd;
-				} else
-					cellValue = reader.getCellValue(currentSheet.getTableName(), position);
-			} while (!typeEnd);
+					position = sc.nextLine();
+					if (position.equals("Fin")) {
+						System.out.println("End");
+						typeEnd = !typeEnd;
+					} else
+						cellValue = odsReader.getCellValue(currentSheet.getTableName(), position);
+				} while (!typeEnd);
+			}
 		}
 	}
 
