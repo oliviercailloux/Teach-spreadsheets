2dca3dec5e756a3ad0d064c604e7bba6ba6d8315
nael93260 <nael93260@hotmail.com>
2021-06-17 21:47:19 +0200

    WriteException

---
+++ src/main/java/io/github/oliviercailloux/teach_spreadsheets/bimodal/WriteException.java
@@ -0,0 +1,14 @@
+package io.github.oliviercailloux.teach_spreadsheets.bimodal;
+
+public class WriteException extends Exception {
+	private static final long serialVersionUID = 6254301468790470484L;
+
+	/**
+	 * Constructs a {@code WriteException} with the specified detail message.
+	 *
+	 * @param message the detail message.
+	 */
+	public WriteException(String message) {
+		super(message);
+	}
+}
